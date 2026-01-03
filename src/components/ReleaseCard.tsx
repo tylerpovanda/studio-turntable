d0fd13858ebc12f844c2d32e326462ea2f812d56
Tyler Povanda <tyler.povanda@gmail.com>
2026-01-02 19:13:18 -0500

    Responsive tweak

--- src/components/ReleaseCard.tsx
+++ src/components/ReleaseCard.tsx
@@ -111,8 +111,8 @@ const ReleaseCard: React.FC<ReleaseCardProps> = ({ releases }) => {
 
   if (!releases || releases.length === 0) return <div>No releases available</div>;
 
-// If height < 720px, show overlay message
-if (windowHeight < 720) {
+// If height < 600px, show overlay message
+if (windowHeight < 599) {
   const titleHeight = 80; // match App.tsx title height
   const availableHeight = windowHeight - titleHeight;
 
@@ -300,7 +300,7 @@ if (windowHeight < 720) {
         justifyContent:
           windowHeight >= 900
             ? "center"
-            : windowHeight >= 720
+            : windowHeight >= 599
             ? "flex-start"
             : "center",
       }}
@@ -310,7 +310,7 @@ if (windowHeight < 720) {
         className="flex flex-col items-center"
         style={{
           marginTop:
-            windowHeight >= 720 && windowHeight < 900
+            windowHeight >= 599 && windowHeight < 900
               ? (windowHeight - titleHeight) / 2 - jacketSize / 2
               : 0,
         }}
